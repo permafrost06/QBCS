3013687553f3778641d680cb656c11ccea843e9e
permafrost06 <zahin06@gmail.com>
2022-12-06 16:42:34 +0600

    added SolaimanLipi font

--- src/App.vue
+++ src/App.vue
@@ -28,9 +28,15 @@ const editMode = ref(false);
 </template>
 
 <style>
+@font-face {
+  font-family: "SolaimanLipi";
+  src: url("fonts/SolaimanLipi.ttf");
+}
+
 body {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica,
-    Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  font-family: "SolaimanLipi", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+    "Segoe UI Symbol";
 }
 
 body {
